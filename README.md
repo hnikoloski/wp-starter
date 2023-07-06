7a45a5913f836e092f5f717427b92ed2b8297525
Hristijan Nikoloski <hristijan.nikoloski@iris-worldwide.com>
2023-07-06 17:17:43 +0200

    Tidy up

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wp-starter
 Wordpress Starter theme Template
+
+## Installation
+```
+npm install
+```
+
+## Development
+``` 
+npm run dev
+```
+
+## Watch
+```
+npm run watch
+```
+
+## Production
+```
+npm run prod
+```
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,27 @@
 // webpack.mix.js
 
 const mix = require("laravel-mix");
-
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 mix
     .options({
         processCssUrls: false,
     })
     .js('src/app.js', 'js')
     .sass('src/app.scss', 'css')
-    .setPublicPath('dist');
+    .setPublicPath('dist')
+    // Example of copying Fonts to dist folder. Comment out if you don't need this or delete it.
+    .copyDirectory('src/sass/fonts/roboto', 'dist/css/fonts')
+    .sourceMaps(true, 'source-map')
 
+    .disableNotifications();
+
+mix.webpackConfig({
+    plugins: [
+        new CompressionWebpackPlugin({
+            algorithm: "gzip",
+            test: /\.js$|\.css$|\.html$|\.svg$/,
+            threshold: 10240,
+            minRatio: 0.8
+        })
+    ]
+});
